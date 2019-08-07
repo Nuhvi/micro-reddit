40e7efc553cdcdac1606cc977fa075f038c97df4
themonster2015 <quynhyen.vothi@gmail.com>
2019-08-07 18:49:17 +0200

    tested user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new()
+  end
+  test "valid user" do
+
+    assert_not @user.valid? 
+  end
 end
